cc68bc992cca97f27c57f09a4a298382c1c9b838
30319-khamphee <30319@sa.ac.th>
2026-04-26 23:27:37 +0700

    Add files via upload

---
+++ A1-001.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+#include <string> 
+using namespace std;
+int main(){ 
+string a,b;
+cin>>a;
+cin>>b;
+cout<<"Hello "<<a<<" "<<b <<endl;
+cout<<a.substr(0,2)<<b.substr(0,2)<<endl;
+	return 0 ;
+}

874ca68a0cc598929dc973347328df617c6bbdcf
30319-khamphee <30319@sa.ac.th>
2026-04-28 23:40:42 +0700

    update a2-007.cpp

---
+++ a2/a2-007.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std ;
+int main(){
+int n,m;
+int i;
+vector<int> a1,a2,w;
+cin>>n>>m;
+for(i=0;i<=n;i++){  //input x begin
+int a;
+cin>>a;
+a1.push_back(a);
+}
+for(i=0;i<=n;i++){ //input x end
+int a;
+cin>>a;
+a2.push_back(a);
+}for(i=0;i<=m;i++){
+int a;
+cin>>a;
+w.push_back(a);
+}
+
+}   

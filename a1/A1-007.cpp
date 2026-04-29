8f4b5685d28ef5bc276b79f08d9d12155592422f
30319-khamphee <30319@sa.ac.th>
2026-04-29 19:13:04 +0700

    Add files via upload

---
+++ a1/A1-007.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+string a;
+cin>>a;
+if(a=="a"||a=="e"||a=="i"||a=="o"||a=="u"){
+	cout<<"yes";
+}else{
+	cout<<"no";
+}
+	return 0 ;
+}

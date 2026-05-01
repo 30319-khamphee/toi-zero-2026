1368f1b2c662f5caeb7504ea0b3822f9bb547018
30319-khamphee <30319@sa.ac.th>
2026-05-01 23:48:07 +0700

    Update a2 base .cpp

--- a2/a2 base .cpp
+++ a2/a2 base .cpp
@@ -1,13 +1,27 @@
+// Write code below 💖
 #include <iostream>
 #include <algorithm>
+#include <vector>
 using namespace std;
 int main (){
     vector<int> b12;
     int n,z;
-    std::cin >> n;
+    cin >> n;
+    z=n;
     while(z>0){
-        if(z%16>10){
-            push_back(z%16);
+        if(z%16<10){
+          int c='0'+(z%16);
+            b12.push_back(c);
+        }else{
+          int c='A'+(z%16-10);
+          b12.push_back(c);
         }
+        z=z/16;
     }
+    reverse(b12.begin(),b12.end());
+    int i;
+    for(i=0;i<b12.size();i++){
+      cout<<char(b12[i]);
+    }
+    return 0;
 }

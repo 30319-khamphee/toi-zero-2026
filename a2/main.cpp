94f8f128dd0a3febb2e0223922ef369e6a14c483
30319-khamphee <30319@sa.ac.th>
2026-05-03 20:14:01 +0700

    update main.cpp

--- a2/main.cpp
+++ a2/main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 int main(){
     int n,in,i;
-    vector <int> s;
+    vector <int> s,p;
     cin>>n;
     for(i=0;i<n;i++){
         cin>>in;
@@ -17,9 +17,17 @@ int main(){
     for(i=0;i<s.size();i++){
          anum=anum+s[i];
     }float a=anum/n;
+    for(i=0;i<s.size();i++){
+        if(s[i]>a){
+            p.push_back(s[i]);
+        }
+    }
     cout<<"Student: ";
     for(i=1;i<=n;i++){
         cout<<"Student"<<i<<" ";
     }cout<<endl;
+    cout<<"Highest score: "<<h<<endl;
+    cout<<"Lowest score: "<<l<<endl;
     return 0;
+    
 }

0a6dbe345eb909e04cfba64cfebb93dbb4574f3a
30319-khamphee <30319@sa.ac.th>
2026-05-07 01:51:02 +0700

    Update A2-007_unfinish.cpp

--- a2/A2-007_unfinish.cpp
+++ a2/A2-007_unfinish.cpp
@@ -3,33 +3,50 @@
 using namespace std ;
 int main(){
 int n,m;
-int i,a,b;
-vector<int> a1,a2,w;
+int i,ina,inb,inw;
+vector<int> a,b,r;
 cin>>n>>m;
 for(i=0;i<=n;i++){  //input x begin
-cin>>a;
-a1.push_back(a);
+cin>>ina;
+a.push_back(ina);
 }
 for(i=0;i<=n;i++){ //input x end
-cin>>a;
-a2.push_back(a);
-}for(i=0;i<=m;i++){ // want bucked
-cin>>b;
-w.push_back(a);
+cin>>inb;
+a.push_back(inb);
+}bool[n]=false
+for(i=0;i<=m;i++){ // want bucked
+cin>>inw;
+bool{inw}=true
 }int nb;
 bool isfoud[n]=false;
 while(true){
-    int bt=-1;
+    int best=-1;
     int max=0;
     int min = 500000;
     for(int i=0;i<=n;i++){
         int cout =0;
-        for()
-    if(){
-     cout+=1
+        for(j=0;i<=n;i++){
+    if(!isfoud[j]){
+     if (A[j] >= A[i] && B[j] <= B[i]) {
+        count++;
     }
+    }
+}
         
+        if(count>max){
+        	max=count;
+        	best=i;
+        	min=b[i]-a[i];
+		}
+		else if(cout>0&&c==max){
+			if(b[i]-a[i]<min){
+				best=i
+				min=b[i]-a[i];
+			}
+		}
     }
+    if(best==1) break;
+    
 }
  return 0;
 }   

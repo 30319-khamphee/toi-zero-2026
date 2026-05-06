cdaf6b714ec55d65f77dc88810d68ece9d6a00ed
30319-khamphee <30319@sa.ac.th>
2026-05-06 15:03:10 +0700

    Update A2-007_unfinish.cpp

--- a2/A2-007_unfinish.cpp
+++ a2/A2-007_unfinish.cpp
@@ -4,21 +4,28 @@ using namespace std ;
 int main(){
 int n,m;
 int i;
-vector<int> a1,a2,w;
+vector<int> a,b,a1,a2,w;
 cin>>n>>m;
 for(i=0;i<=n;i++){  //input x begin
-int a;
 cin>>a;
 a1.push_back(a);
 }
 for(i=0;i<=n;i++){ //input x end
-int a;
 cin>>a;
 a2.push_back(a);
-}for(i=0;i<=m;i++){
-int a;
-cin>>a;
+}for(i=0;i<=m;i++){ // want bucked
+cin>>b;
 w.push_back(a);
+}int i,nb;
+bool foud= false;
+for(i=0;i<n;i++){
+    for(int j=0;j<want.size();j++){
+    if(a1[i]>=a1[w[j]]&&a2[i]<=a2[w[j]]){
+     
+     foud=false;
+    }
+        
+    }
 }
-
+ return 0;
 }   
